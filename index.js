0a47a851d8be650d2433d8cf6c377f41161e04e5
yuxi Shi <ys3599@tc.columbia.edu>
2024-05-30 00:14:35 -0400

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name: "Yuxi Shi",
+    streetAddress: "50 W 97th St"
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: _, ...rest } = employee;
+    return rest;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
